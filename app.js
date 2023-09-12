d2b60061ba0fba7a560f0248bae215bc27ef5b15
dontbecypher16 <43769645+dontbecypher16@users.noreply.github.com>
2023-09-12 10:48:55 -0400

    Add packages for auth features and development ease.

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
 const { testDbConnection } = require("./config/db");
-testDbConnection();
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const csurf = require("csurf");
+//const session = require("express-session");
+const createError = require("http-errors");
+const path = require("path");
+const passport = require("passport");
 
+const app = express();
+testDbConnection();
 dotenv.config();
 const port = 4000;
-app.set("views", "./views");
-
-app.use(express.static(__dirname + "/public/images"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 const Contact = require("./routes/contacts");
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//read what to add to session such as secret, etc
+//app.use(session());
+app.use(csurf());
+
 app.use(Contact);
+
+app.use(function (req, res) {
+  next(createError(404));
+});
 
 app.listen(port, () => {
   console.log(`Server listening on localhost:${port}`);
